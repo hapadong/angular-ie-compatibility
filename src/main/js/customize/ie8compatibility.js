119cfa86c707c3e49335dac8cb714f1fcf3449d8
Dong Wang <dong.wangd5@gmail.com>
2014-11-19 21:29:40 -0600

    Set frame
    Frame: Home; about
    Home page: introduction and issues
    Issue: typeahead done

---
+++ src/main/js/customize/ie8compatibility.js
@@ -0,0 +1,45 @@
+var iec = angular.module("ie8compatibility", ['ngRoute', 'ui.bootstrap']);
+
+iec.config(function($routeProvider){
+    $routeProvider.when('/home', {
+            controller: 'iecCtrl',
+            templateUrl: 'views/home.html'
+        }
+    ).when('/about', {
+            controller: 'iecCtrl',
+            templateUrl: 'views/about.html'
+        }
+    ).otherwise({redirectTo: '/home'});
+});
+
+iec.controller("iecCtrl", function($scope, $location){
+    $scope.templates = [
+        {
+            name: 'introduction',
+            url: 'views/introduction.html',
+            description: 'introduction'
+        },
+        {
+            name: 'AngularJS UI Bootstrap typeahead',
+            url: 'views/issues/typeahead.html',
+            description: 'typeahead issue'
+        },
+        {
+            name: 'AngularJS UI Bootstrap modal window size',
+            url: 'views/issues/modal.html',
+            description: 'modal size'
+        }
+    ];
+
+    $scope.template = $scope.templates[0];
+    $scope.activeTemplate = function(template){
+        $scope.template = template;
+    }
+
+    $scope.isActive = function(link) {
+        if (link == $location.path()){
+            return true;
+        }
+        return false;
+    }
+});
